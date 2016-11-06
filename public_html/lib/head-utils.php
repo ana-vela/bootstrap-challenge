2a2d166e0827d08556892af8faf5fdf37e48f681
Ana Vela <avela7@cnm.edu>
2016-11-06 12:59:52 -0700

    added css dir and stylesheet, removed extra lines in head-utils.php

--- public_html/lib/head-utils.php
+++ public_html/lib/head-utils.php
@@ -1,17 +1,11 @@
-<meta charset="UTF-8"/>
-<meta name
-
-<!DOCTYPE html>
-<html lang="en">
-	<head>
 		<meta charset="utf-8">
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-		<title>Bootstrap 101 Template</title>
+		<title>My Bootstrap Challenge</title>
 
 		<!-- Bootstrap -->
-		<link href="css/bootstrap.min.css" rel="stylesheet">
+		<link rel="stylesheet" href="css/style.css" type="text/css">
 
 		<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
